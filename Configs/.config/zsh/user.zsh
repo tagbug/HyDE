f57cb2e7df65a996a48b2af357986f075dc43d47
tagbug <41150861+tagbug@users.noreply.github.com>
2025-07-26 21:00:58 +0800

    style(configs): update configurations for consistency and usability

--- Configs/.config/zsh/user.zsh
+++ Configs/.config/zsh/user.zsh
@@ -1,18 +1,18 @@
 #  Startup 
 # Commands to execute on startup (before the prompt is shown)
 # Check if the interactive shell option is set
-if [[ $- == *i* ]]; then
-    # This is a good place to load graphic/ascii art, display system information, etc.
-    if command -v pokego >/dev/null; then
-        pokego --no-title -r 1,3,6
-    elif command -v pokemon-colorscripts >/dev/null; then
-        pokemon-colorscripts --no-title -r 1,3,6
-    elif command -v fastfetch >/dev/null; then
-        if do_render "image"; then
-            fastfetch --logo-type kitty
-        fi
-    fi
-fi
+# if [[ $- == *i* ]]; then
+#     # This is a good place to load graphic/ascii art, display system information, etc.
+#     if command -v pokego >/dev/null; then
+#         pokego --no-title -r 1,3,6
+#     elif command -v pokemon-colorscripts >/dev/null; then
+#         pokemon-colorscripts --no-title -r 1,3,6
+#     elif command -v fastfetch >/dev/null; then
+#         if do_render "image"; then
+#             fastfetch --logo-type kitty
+#         fi
+#     fi
+# fi
 
 #   Overrides 
 # HYDE_ZSH_NO_PLUGINS=1 # Set to 1 to disable loading of oh-my-zsh plugins, useful if you want to use your zsh plugins system 
